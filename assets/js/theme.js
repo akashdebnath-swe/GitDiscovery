d7699347137443bca410207c3beb238987a41ebe
asyncawai <akashdebnathwd@gmail.com>
2024-06-21 21:29:11 +0530

    Added light and dark theme mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,3 +5,31 @@
  */
 
 'use strict';
+
+const $HTML = document.documentElement; /** NodeElement */
+const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const theme = sessionStorage.getItem('theme');
+
+if (theme) {
+    $HTML.dataset.theme = theme;
+} else {
+    $HTML.dataset.theme = isDark ? 'dark' : 'light';
+}
+
+let isPressed = false;
+
+const changeTheme = function () {
+    isPressed = isPressed ? false : true;
+    this.setAttribute('aria-pressed', isPressed);
+    $HTML.setAttribute(
+        'data-theme',
+        $HTML.dataset.theme === 'light' ? 'dark' : 'light'
+    );
+    sessionStorage.setItem('theme', $HTML.dataset.theme);
+};
+
+window.addEventListener('load', function () {
+    const $themeBtn = document.querySelector('[data-theme-btn]');
+
+    $themeBtn.addEventListener('click', changeTheme);
+});
