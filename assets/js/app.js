93f2405b83a4eb0f9e5b0191b662d10814ae1acd
asyncawai <akashdebnathwd@gmail.com>
2024-06-22 10:10:14 +0530

    Added interactivity to header search field

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,51 @@
  */
 
 'use strict';
+
+/**
+ *
+ * Variables starting with $ sign are all NodeElement (DOM Element).
+ *
+ */
+
+/**
+ *  Add eventlistener on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType String
+ * @param {Function} callback Function
+ */
+
+const addEventOnElements = function ($elements, eventType, callback) {
+    for (const $item of $elements) {
+        $item.addEventListener(eventType, callback);
+    }
+};
+
+/**
+ *
+ * Header scroll state
+ *
+ */
+
+const $header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', function () {
+    $header.classList[this.window.scrollY > 50 ? 'add' : 'remove']('active');
+});
+
+/**
+ *
+ * Search toggle
+ *
+ */
+
+const $searchToggler = document.querySelector('[data-search-toggler]');
+const $searchField = document.querySelector('[data-search-field]');
+let isExpanded = false;
+
+$searchToggler.addEventListener('click', function () {
+    $header.classList.toggle('search-active');
+    isExpanded = isExpanded ? false : true;
+    this.setAttribute('aria-expanded', isExpanded);
+    $searchField.focus();
+});
