727ea0f6971836647df22328e66d7be354e525a1
asyncawai <akashdebnathwd@gmail.com>
2024-06-23 20:19:13 +0530

    Created updateRepository function and render repositories of the user

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@
 "use strict";
 
 import { fetchData } from "./api.js";
+import { numberToKilo } from "./module.js";
 
 /**
  *
@@ -202,116 +203,119 @@ window.updateProfile = function (profileUrl) {
             followingUrl = following_url.replace("{/other_user}", "");
 
             $profileCard.innerHTML = `
-            <figure class="${
-                type === "User" ? "avatar-circle" : "avatar-rounded"
-            } img-holder" style="--width: 280; --height: 280">
-                <img 
-                    src="${avatar_url}"
-                    width="280"
-                    height="280"
-                    alt="${username}"
-                    class="img-cover"
-                />
-            </figure>
-
-            ${name ? `<h1 class="title-2">${name}</h1>` : ""}
-
-            <p class="username text-primary">${username}</p>
-
-            ${bio ? `<p class="bio">${bio}</p>` : ""}
-
-            <span class="btn btn-secondary">
-                <span
-                    class="material-symbols-rounded"
-                    aria-hidden="true"
-                    >open_in_new</span
-                >
-                <a
-                    href="${githubPageUrl}"
-                    target="_blank"
-                    class="span"
-                    >See on Github</a
-                >
-            </span>
-
-            <ul class="profile-meta">
-                ${
-                    company
-                        ? `
-                    <li class="meta-item">
-                        <span class="material-symbols-rounded" aria-hidden="true">apartment</span>
-                        <span class="meta-text">${company}</span>
-                    </li>`
-                        : ""
-                }
-                ${
-                    location
-                        ? `
-                    <li class="meta-item">
-                        <span class="material-symbols-rounded" aria-hidden="true">location_on</span>
-                        <span class="meta-text">${location}</span>
-                    </li>`
-                        : ""
-                }
-                ${
-                    website
-                        ? `
-                    <li class="meta-item">
-                        <span class="material-symbols-rounded" aria-hidden="true">captive_portal</span>
-                        <a href="${website}" target="_blank" class="meta-text">${website}</a>
-                    </li>`
-                        : ""
-                }
-                ${
-                    twitter_username
-                        ? `
-                    <li class="meta-item">
-                        <span class="icon">
-                            <svg
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M19.9441 7.92638C19.9568 8.10403 19.9568 8.28173 19.9568 8.45938C19.9568 13.8781 15.8325 20.1218 8.29441 20.1218C5.97207 20.1218 3.81473 19.4492 2 18.2817C2.32996 18.3198 2.64719 18.3325 2.98984 18.3325C4.90605 18.3325 6.67004 17.6853 8.07867 16.5812C6.27664 16.5431 4.76648 15.3629 4.24617 13.7386C4.5 13.7766 4.75379 13.802 5.02031 13.802C5.38832 13.802 5.75637 13.7512 6.09898 13.6624C4.22082 13.2817 2.81215 11.632 2.81215 9.63958V9.58884C3.35781 9.89341 3.99238 10.0838 4.66492 10.1091C3.56086 9.37306 2.83754 8.11673 2.83754 6.6954C2.83754 5.93399 3.04055 5.23603 3.3959 4.62688C5.41367 7.11419 8.44668 8.73853 11.8477 8.91622C11.7842 8.61165 11.7461 8.29442 11.7461 7.97716C11.7461 5.71825 13.5736 3.87817 15.8451 3.87817C17.0253 3.87817 18.0913 4.3731 18.84 5.17259C19.7664 4.99493 20.6547 4.65228 21.4416 4.18274C21.137 5.13454 20.4898 5.93403 19.6395 6.44161C20.4644 6.35282 21.2639 6.12435 21.9999 5.80712C21.4416 6.61927 20.7436 7.34259 19.9441 7.92638Z"
-                                    fill="var(--on-background)"
-                                ></path>
-                            </svg>
-                        </span>
+                    <figure class="${
+                        type === "User" ? "avatar-circle" : "avatar-rounded"
+                    } img-holder" style="--width: 280; --height: 280">
+                        <img 
+                            src="${avatar_url}"
+                            width="280"
+                            height="280"
+                            alt="${username}"
+                            class="img-cover"
+                        />
+                    </figure>
+                
+                    ${name ? `<h1 class="title-2">${name}</h1>` : ""}
+                
+                    <p class="username text-primary">${username}</p>
+                
+                    ${bio ? `<p class="bio">${bio}</p>` : ""}
+                
+                    <span class="btn btn-secondary">
+                        <span
+                            class="material-symbols-rounded"
+                            aria-hidden="true"
+                            >open_in_new</span
+                        >
                         <a
-                            href="https://x.com/${twitter_username}"
+                            href="${githubPageUrl}"
                             target="_blank"
-                            class="meta-text"
-                            >@${twitter_username}</a>
-                    </li>
-                    `
-                        : ""
-                }
-            </ul>
+                            class="span"
+                            >See on Github</a
+                        >
+                    </span>
+                
+                    <ul class="profile-meta">
+                        ${
+                            company
+                                ? `
+                            <li class="meta-item">
+                                <span class="material-symbols-rounded" aria-hidden="true">apartment</span>
+                                <span class="meta-text">${company}</span>
+                            </li>`
+                                : ""
+                        }
+                        ${
+                            location
+                                ? `
+                            <li class="meta-item">
+                                <span class="material-symbols-rounded" aria-hidden="true">location_on</span>
+                                <span class="meta-text">${location}</span>
+                            </li>`
+                                : ""
+                        }
+                        ${
+                            website
+                                ? `
+                            <li class="meta-item">
+                                <span class="material-symbols-rounded" aria-hidden="true">captive_portal</span>
+                                <a href="${website}" target="_blank" class="meta-text">${website}</a>
+                            </li>`
+                                : ""
+                        }
+                        ${
+                            twitter_username
+                                ? `
+                            <li class="meta-item">
+                                <span class="icon">
+                                    <svg
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M19.9441 7.92638C19.9568 8.10403 19.9568 8.28173 19.9568 8.45938C19.9568 13.8781 15.8325 20.1218 8.29441 20.1218C5.97207 20.1218 3.81473 19.4492 2 18.2817C2.32996 18.3198 2.64719 18.3325 2.98984 18.3325C4.90605 18.3325 6.67004 17.6853 8.07867 16.5812C6.27664 16.5431 4.76648 15.3629 4.24617 13.7386C4.5 13.7766 4.75379 13.802 5.02031 13.802C5.38832 13.802 5.75637 13.7512 6.09898 13.6624C4.22082 13.2817 2.81215 11.632 2.81215 9.63958V9.58884C3.35781 9.89341 3.99238 10.0838 4.66492 10.1091C3.56086 9.37306 2.83754 8.11673 2.83754 6.6954C2.83754 5.93399 3.04055 5.23603 3.3959 4.62688C5.41367 7.11419 8.44668 8.73853 11.8477 8.91622C11.7842 8.61165 11.7461 8.29442 11.7461 7.97716C11.7461 5.71825 13.5736 3.87817 15.8451 3.87817C17.0253 3.87817 18.0913 4.3731 18.84 5.17259C19.7664 4.99493 20.6547 4.65228 21.4416 4.18274C21.137 5.13454 20.4898 5.93403 19.6395 6.44161C20.4644 6.35282 21.2639 6.12435 21.9999 5.80712C21.4416 6.61927 20.7436 7.34259 19.9441 7.92638Z"
+                                            fill="var(--on-background)"
+                                        ></path>
+                                    </svg>
+                                </span>
+                                <a
+                                    href="https://x.com/${twitter_username}"
+                                    target="_blank"
+                                    class="meta-text"
+                                    >@${twitter_username}</a>
+                            </li>
+                            `
+                                : ""
+                        }
+                    </ul>
+                
+                    <div class="profile-stats">
+                        <li class="stats-item">
+                            <span class="body">${
+                                public_repos ? numberToKilo(public_repos) : 0
+                            }</span> Repos
+                        </li>
+                        <li class="stats-item">
+                            <span class="body">${
+                                followers ? numberToKilo(followers) : 0
+                            }</span> Followers
+                        </li>
+                        <li class="stats-item">
+                            <span class="body">${
+                                following ? numberToKilo(following) : 0
+                            }</span> Following
+                        </li>
+                    </div>
+                
+                    <div class="footer">
+                        <p class="copyright">&copy; 2024 skies-codes</p>
+                    </div>
+                `;
 
-            <div class="profile-stats">
-                <li class="stats-item">
-                    <span class="body">${
-                        public_repos ? public_repos : 0
-                    }</span> Repos
-                </li>
-                <li class="stats-item">
-                    <span class="body">${
-                        followers ? followers : 0
-                    }</span> Followers
-                </li>
-                <li class="stats-item">
-                    <span class="body">${
-                        following ? following : 0
-                    }</span> Following
-                </li>
-            </div>
-
-            <div class="footer">
-                <p class="copyright">&copy; 2024 skies-codes</p>
-            </div>
-        `;
+            // Update repository
+            updateRepository();
         },
         () => {
             $error.style.display = "grid";
@@ -328,3 +332,102 @@ window.updateProfile = function (profileUrl) {
 };
 
 updateProfile(apiUrl);
+
+/**
+ * Repository
+ */
+
+let forkedRepos = [];
+
+// ${repoUrl}?sort=created&per_page=12
+
+const updateRepository = function () {
+    fetchData(`${repoUrl}?sort=created`, function (data) {
+        $repoPanel.innerHTML = `<h2 class="sr-only">Repositories</h2>`;
+        forkedRepos = data.filter((repo) => repo.fork);
+
+        const repositories = data.filter((repo) => !repo.fork);
+
+        if (repositories.length) {
+            for (const repo of repositories) {
+                const {
+                    name,
+                    html_url: website,
+                    description,
+                    private: isPrivate,
+                    language,
+                    stargazers_count: stars_count,
+                    forks_count,
+                } = repo;
+
+                const $repoCard = document.createElement("article");
+                $repoCard.classList.add("card", "repo-card");
+
+                $repoCard.innerHTML = `
+                    <div class="card-body">
+                        <a href="${website}" target="_blank" class="card-title">
+                            <h3 class="title-3">${name}</h3>
+                        </a>
+
+                        ${
+                            description
+                                ? `<p class="card-text">${description}</p>`
+                                : ""
+                        }
+
+                        <span class="badge">${
+                            isPrivate ? "Private" : "Public"
+                        }</span>
+                    </div>
+
+                    <div class="card-footer">
+                    ${
+                        language
+                            ? `
+                        <div class="meta-item">
+                            <span
+                                class="material-symbols-rounded"
+                                aria-hidden="true"
+                                >code_blocks</span
+                            >
+                            <span>${language}</span>
+                        </div>`
+                            : ""
+                    }
+                        <div class="meta-item">
+                            <span
+                                class="material-symbols-rounded"
+                                aria-hidden="true"
+                                >star_rate</span
+                            >
+                            <span>${numberToKilo(stars_count)}</span>
+                        </div>
+                        <div class="meta-item">
+                            <span
+                                class="material-symbols-rounded"
+                                aria-hidden="true"
+                                >family_history</span
+                            >
+                            <span>${numberToKilo(forks_count)}</span>
+                        </div>
+                    </div>
+                 `;
+
+                $repoPanel.appendChild($repoCard);
+            }
+        } else {
+            $repoPanel.innerHTML = `
+                <div class="error-content">
+                    <p class="title-1">Oops! :(</p>
+                    <p class="text">
+                        Doesn't have any public repositories yet.
+                    </p>
+                </div>
+            `;
+        }
+    });
+};
+
+/**
+ * Forked Repository
+ */
--- assets/js/module.js
+++ assets/js/module.js
@@ -4,4 +4,23 @@
  * @copyright skies-codes 2024
  */
 
-'use strict';
+"use strict";
+
+/**
+ *
+ * @param {*} number Number
+ */
+
+export const numberToKilo = function (number) {
+    const numStr = String(number);
+
+    if (numStr.length <= 3) {
+        return numStr;
+    } else if (numStr.length >= 4 && numStr.length <= 5) {
+        return `${numStr.slice(0, -3)}.${numStr.slice(-3, -2)}k`;
+    } else if (numStr.length === 6) {
+        return `${numStr.slice(0, -3)}k`;
+    } else {
+        return `${numStr.slice(0, -6)}M`;
+    }
+};
