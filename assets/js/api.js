bee67d71033303ba446018cd6327264aebe06349
asyncawai <akashdebnathwd@gmail.com>
2024-06-23 11:57:16 +0530

    Created fetchData function in api.js file

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,4 +4,23 @@
  * @copyright skies-codes 2024
  */
 
-'use strict';
+"use strict";
+
+/**
+ *
+ * @param {*} url API URL [required]
+ * @param {*} successCallback Success callback [required]
+ * @param {*} errorCallback Error callback [optional]
+ */
+
+export async function fetchData(url, successCallback, errorCallback) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        successCallback(data);
+    } else {
+        const error = await response.json();
+        errorCallback && errorCallback(error);
+    }
+}
